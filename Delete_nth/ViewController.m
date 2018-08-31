d3af15633f801153975ae557507783e418b8b390
penghe <penghe@wondersgroup.com>
2018-08-31 16:08:10 +0800

    第二种方法

--- Delete_nth/ViewController.m
+++ Delete_nth/ViewController.m
@@ -40,6 +40,7 @@
     // Dispose of any resources that can be recreated.
 }
 // Time complexity O(n^2)
+/*
 - (NSArray *)delete_nth_naiveByArray:(NSArray *)inArray n:(NSInteger)n
 {
     NSMutableArray *restultsArray = [NSMutableArray array];
@@ -57,5 +58,39 @@
     }
     return [restultsArray copy];
 }
+*/
+//http://kodango.com/understand-defaultdict-in-python
+//Time Complexity O(n), using hash tables.
+- (NSArray *)delete_nth_naiveByArray:(NSArray *)inArray n:(NSInteger)n
+{
 
+    NSMutableArray *restultsArray = [NSMutableArray array];
+    NSArray *tempArray = [inArray copy];
+    //用hashtable 存储
+    NSMutableDictionary *tempDic = [NSMutableDictionary dictionary];
+    //用数组的的元素作为key存放在字典中
+    for (id obj in tempArray)
+    {
+        //如果字典的keys不包含 这个数组的元素 那么value是1
+        if (![tempDic.allKeys containsObject:obj])
+        {
+            tempDic[obj] = @1;
+            [restultsArray addObject:obj];
+
+        }
+        else
+        {
+            //如果字典obj为key的value小于n 那么obj的key对应的 value+1
+            if ([tempDic[obj] integerValue] < n)
+            {
+                [restultsArray addObject:obj];
+                tempDic[obj] = @([tempDic[obj] integerValue] + 1);
+
+            }
+
+        }
+
+    }
+    return [restultsArray copy];
+}
 @end
